bebea292c5217dacafff5902505909658b45548d
EnityBlackHawk <97620478+EnityBlackHawk@users.noreply.github.com>
2023-12-01 16:27:56 -0300

    LAN-68 Features (#4)
    * LAN-129 feat: Add FeatureCards components
    
    * LAN-129 refactor: Remove flex hover effect

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,45 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		screens: {
-			'Mobile': {'max': '575px'},
-			// => @media (max-width: 575px) { ... }
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      s_mobile: "445px",
+      lg: "1024px",
+      md: "768px",
+      Mobile: { max: "575px" },
+      // => @media (max-width: 575px) { ... }
 
-			'TabletPortrait': {'max':'992px'},
-			// => @media (min-width: 575px, max-width: 992px) { ... }
+      TabletPortrait: { max: "992px" },
+      // => @media (min-width: 575px, max-width: 992px) { ... }
 
-			'TabletLandscape': '992px',
-			// => @media (min-width: 992px) { ... }
-		},
-		extend: {
-			colors: {
-				green: "#26D07C",
-				darkblue: "#002B49",
-				blue: "#003C71",
-				lightblue: "#0080AE",
-				gray: "#BABEBF",
-				lightgray: "#E7E9E7",
-				white: "#FFFFFF",
-			},
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-			},
-		},
-	},
-	plugins: [],
+      TabletLandscape: "992px",
+      // => @media (min-width: 992px) { ... }
+    },
+    extend: {
+      colors: {
+        green: "#26D07C",
+        darkblue: "#002B49",
+        blue: "#003C71",
+        lightblue: "#0080AE",
+        gray: "#BABEBF",
+        lightgray: "#E7E9E7",
+        white: "#FFFFFF",
+      },
+      dropShadow: {
+        base: "0 0 25px #26D07C",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
 };
 export default config;
