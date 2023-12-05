4ba1f51b34051ad340e8845c75633b4eeacc56f3
Gustavo Kramer <gustavokramer123@gmail.com>
2023-12-05 16:33:18 -0300

    LAN-137-LAN-140 refactor: add responsiveness

--- src/components/cases/index.tsx
+++ src/components/cases/index.tsx
@@ -4,8 +4,8 @@ import { PrismicNextImage } from "@prismicio/next";
 import { useState } from "react";
 
 export default function Cases({ slice }: CasesSliceProps) {
-  const [selected, setSelected] = useState(1);
   const middleElementIndex = Math.floor(slice.items.length / 2);
+  const [selected, setSelected] = useState(middleElementIndex);
   const indexTotal = slice.items.length - 1;
 
   const MakeTranslation = (value: number) => {
@@ -14,7 +14,7 @@ export default function Cases({ slice }: CasesSliceProps) {
   };
 
   const CalcTranslation = (value: number) =>
-    (middleElementIndex - value) * 33.33;
+    (middleElementIndex - value) * Math.ceil(100 / slice.items.length);
 
   const toRight = () => {
     const value = selected == indexTotal ? 0 : selected + 1;
@@ -29,18 +29,12 @@ export default function Cases({ slice }: CasesSliceProps) {
   };
 
   return (
-    <div className="w-full h-fit py-[100px] TabletPortrait:p-[0px] flex-col justify-center items-center gap-8 flex bg-white overflow-hidden">
-      <button className="bg-blue" onClick={toLeft}>
-        click -
-      </button>
-      <button className="bg-blue" onClick={toRight}>
-        click +
-      </button>
+    <div className="w-full h-fit py-24 flex-col justify-center items-center gap-8 flex bg-white overflow-hidden">
       <div className="flex-col justify-center items-center flex">
         <div className="text-green text-sm font-semibold uppercase tracking-[8.26px]">
           {slice?.primary.subtitle}
         </div>
-        <div className="text-blue text-4xl font-bold mb-[32px] TabletPortrait:mb-[0px]">
+        <div className="text-blue text-4xl font-bold mb-8">
           {slice?.primary.title}
         </div>
       </div>
@@ -53,14 +47,14 @@ export default function Cases({ slice }: CasesSliceProps) {
             <div
               className={`${
                 selected == i ? "scale-110" : "blur-sm"
-              } justify-center items-center gap-8 flex w-[40vw] mx-10 transition-all duration-700`}
+              } justify-center items-center gap-8 flex w-[40vw] Mobile:w-[87vw] TabletPortrait:w-[87vw] TabletLandscape:w-[60vw] mx-10 transition-all duration-700`}
               id={`card-${i}`}
             >
               <div className="w-full h-full overflow-hidden rounded-2xl ">
                 <div className="grid w-auto h-full">
                   <PrismicNextImage
                     field={e.image}
-                    className="w-full h-full aspect-video object-fill row-start-1 row-end-2 col-start-1 col-end-2"
+                    className="w-full h-full aspect-video Mobile:aspect-square object-fill row-start-1 row-end-2 col-start-1 col-end-2"
                   />
                   <div
                     className="row-start-1 row-end-2 col-start-1 col-end-2 relative h-109"
@@ -69,19 +63,25 @@ export default function Cases({ slice }: CasesSliceProps) {
                         "linear-gradient(180deg, rgba(0, 0, 0, 0.58) 0%, rgba(0, 0, 0, 0.46) 100%);",
                     }}
                   >
-                    <div className="m-4">
-                      <PrismicNextImage
-                        field={e.lefticon}
-                        className="inline "
-                      />
-                      <label className=" ml-1">{e.lefticontext}</label>
-                      <label className="float-right ml-1">
-                        {e.rigthicontext}
-                      </label>
-                      <PrismicNextImage
-                        field={e.rigthicon}
-                        className="inline float-right"
-                      />
+                    <div className="m-4 flex justify-between">
+                      <div>
+                        <PrismicNextImage
+                          field={e.lefticon}
+                          className="inline "
+                        />
+                        <label className="Mobile:text-xs ml-1">
+                          {e.lefticontext}
+                        </label>
+                      </div>
+                      <div>
+                        <PrismicNextImage
+                          field={e.rigthicon}
+                          className="inline"
+                        />
+                        <label className=" Mobile:text-xs ml-1">
+                          {e.rigthicontext}
+                        </label>
+                      </div>
                     </div>
                     <div
                       className="w-full absolute bottom-0"
@@ -90,11 +90,13 @@ export default function Cases({ slice }: CasesSliceProps) {
                           "linear-gradient(180deg, rgba(0, 0, 0, 0.70) 0%, rgba(0, 0, 0, 0.70) 100%);",
                       }}
                     >
-                      <div className="m-[5vh] text-center">
-                        <label className="text-xl font-bold uppercase tracking-widest ">
+                      <div className=" text-center">
+                        <label className="text-xl Mobile:text-base font-bold uppercase tracking-widest ">
                           {e.title}
                         </label>
-                        <p className="pt-3">{e.text}</p>
+                        <p className="pt-3 text-base Mobile:p-2 Mobile:text-xs">
+                          {e.text}
+                        </p>
                       </div>
                     </div>
                   </div>
@@ -103,6 +105,20 @@ export default function Cases({ slice }: CasesSliceProps) {
             </div>
           );
         })}
+      </div>
+      <div className="flex">
+        <button
+          className="bg-green rounded-full w-10 h-10 sm:w-12 sm:h-12 mr-2 sm:mr-4 flex items-center justify-center focus:outline-none"
+          onClick={toLeft}
+        >
+          <span className="text-xl sm:text-2xl">&#10094;</span>
+        </button>
+        <button
+          className="bg-green rounded-full w-10 h-10 sm:w-12 sm:h-12 mr-2 sm:mr-4 flex items-center justify-center focus:outline-none"
+          onClick={toRight}
+        >
+          <span className="text-xl sm:text-2xl">&#10095;</span>
+        </button>
       </div>
     </div>
   );
