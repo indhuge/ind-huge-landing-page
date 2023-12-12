72435d78db0bf53a3962537beddbf2d45b2307eb
Luan Felipe <luanf2003@gmail.com>
2023-12-12 11:20:21 -0300

    feat: Add Prismic Preview Support

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -14,14 +14,10 @@ export const repositoryName = config.repositoryName;
  */
 // TODO: Update the routes array to match your project's route structure.
 const routes: prismic.ClientConfig["routes"] = [
-	{
-		type: "landing_page",
-		path: "/",
-	},
-	{
-		type: "landing_page",
-		path: "/:uid",
-	},
+  {
+    type: "landing_page",
+    path: "/",
+  },
 ];
 
 /**
@@ -31,20 +27,20 @@ const routes: prismic.ClientConfig["routes"] = [
  * @param config - Configuration for the Prismic client.
  */
 export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
-	const client = prismic.createClient(repositoryName, {
-		routes,
-		fetchOptions:
-			process.env.NODE_ENV === "production"
-				? { next: { tags: ["prismic"] }, cache: "force-cache" }
-				: { next: { revalidate: 5 } },
-		...config,
-	});
+  const client = prismic.createClient(repositoryName, {
+    routes,
+    fetchOptions:
+      process.env.NODE_ENV === "production"
+        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
+        : { next: { revalidate: 5 } },
+    ...config,
+  });
 
-	prismicNext.enableAutoPreviews({
-		client,
-		previewData: config.previewData,
-		req: config.req,
-	});
+  prismicNext.enableAutoPreviews({
+    client,
+    previewData: config.previewData,
+    req: config.req,
+  });
 
-	return client;
+  return client;
 };
