214ef36a7d2be995bac04fe997067a97132dbd3d
EntityBlackHawk <luanf2003@gmail.com>
2024-02-09 10:42:49 -0300

    LAN-206 refactor: Add Newsletter integration to Prismic

--- src/app/blog/[id]/service.ts
+++ src/app/blog/[id]/service.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@/prismicio";
+import { getBlogPage } from "../service";
 
 export async function GetBlogPage(id: string) {
   const client = createClient();
@@ -9,4 +10,10 @@ export async function GetBlogPage(id: string) {
 export async function GetCategories() {
   const client = createClient();
   return await client.getAllByType("category");
+}
+
+export async function GetNewsletter()
+{
+  const blog = await getBlogPage();
+  return blog.data.slices.find((e) => e.slice_type == "newsletter");
 }
