8c5e26537454bcec0a9838fda51bb8d5385a6b0b
Caiki <caiki2003@gmail.com>
2024-02-24 15:50:26 -0300

    Feat: Metadata LAN-258

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -10,21 +10,9 @@ import Head from "next/head";
 export default async function BlogPage({ params }: { params: { id: string } }) {
   const tPage = await GetBlogPage(params.id);
   const tCategories = GetCategories();
-  const newsletterInfo = GetNewsletter()
-  //const metaimage = require(tPage?.data?.meta_image as string)
-  return (
-    <>
-      <Head>
-        <title>{tPage?.data?.meta_title}</title>
-        <meta name="description" content={tPage?.data?.meta_description as string} />
-        {/*<meta property="og:image" content={metaimage} />
-        <meta property="image" content={metaimage} />*/}
-        <meta property="og:description" content={tPage?.data?.meta_description as string} />
-        <meta property="og:title" content={tPage?.data?.meta_title as string} />
-      </Head>
-      <BlogPost post={await tPage} categories={await tCategories} newsletter={(await newsletterInfo)!!} />
-    </>
-  );
+  const newsletterInfo = GetNewsletter();
+
+  return <BlogPost post={await tPage} categories={await tCategories} newsletter={(await newsletterInfo)!!} />;
 }
 
 export async function generateMetadata(
