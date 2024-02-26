12e7a894e2908acc357f3cd003a4d609d5c37207
Gustavo Kramer <gustavokramer123@gmail.com>
2024-02-25 22:31:58 -0300

    feat add highlited blogpost data into blogcarousel

--- src/components/BlogCarousel/index.tsx
+++ src/components/BlogCarousel/index.tsx
@@ -2,12 +2,24 @@
 import { CasesSliceProps } from "@/slices/CasesSlice";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { useEffect, useState } from "react";
+import { BlogPost } from "../BlogCard";
+import { getHighlightedPosts } from "../HighlightedPosts/service";
+import { BlogPostDocument, CategoryDocument } from "../../../prismicio-types";
 
 export default function Cases({ slice }: any) {
   const middleElementIndex = Math.floor(slice.items.length / 2);
   const [selected, setSelected] = useState(middleElementIndex);
   const indexTotal = slice.items.length - 1;
   const [interval, _setInterval] = useState<NodeJS.Timeout | null>();
+
+  const [pages, setPages] = useState<BlogPostDocument<string>[]>();
+
+  useEffect(() => {
+    getHighlightedPosts().then((e) => {
+      setPages(e);
+    });
+  }, []);
+  
   
   const MakeTranslation = (value: number) => {
     var e = document.getElementById("cardHolder");
@@ -45,46 +57,42 @@ export default function Cases({ slice }: any) {
   return (
     <div className="w-full h-fit py-24 Mobile:pt-4 Mobile:pb-0 flex-col justify-center items-center gap-8 flex Mobile:gap-0 bg-white overflow-hidden relative">
       <div
-        className={`w-fit flex-row flex gap-8 Mobile:gap-0 Mobile:pb-8 transition-all duration-700 transform-gpu`}
+        className={`w-fit h-fit flex-row flex gap-8 Mobile:gap-0 Mobile:pb-8 transition-all duration-700 transform-gpu`}
         id="cardHolder"
       >
-        {slice.items.map((e:any, i:number) => {
+        {pages?.map((e:any, i:number) => {
           return (
             <div 
               key={i}
               className={`${
                 selected == i ? "scale-110" : "blur-sm"
-              } justify-center items-center gap-8 Mobile:gap-0 flex w-[80vw] Mobile:w-[100vw] Laptop:w-[85vw] mx-10 Mobile:mx-0 transition-all duration-700`}
+              } overflow-hidden rounded-2xl Mobile:rounded-none gap-8 Mobile:gap-0 flex h-[40vh] w-[80vw] Mobile:w-[100vw] Laptop:w-[85vw] mx-10 Mobile:mx-0 transition-all duration-700`}
               id={`card-${i}`}
             >
-              <div className="w-full h-full overflow-hidden rounded-2xl Mobile:rounded-none">
-                <div className="grid w-auto h-full">
-                  <PrismicNextImage
-                    alt=""
-                    field={e.image}
-                    className="w-full h-full Mobile:aspect-square object-fill row-start-1 row-end-2 col-start-1 col-end-2"
-                  />
-                  <div
-                    className="row-start-1 row-end-2 col-start-1 col-end-2 relative h-109 flex items-center justify-center bg-black bg-opacity-50">
-                      <div className="w-[50%] Mobile:w-[80%] text-center">
-                        <label className="text-3xl Mobile:text-2xl font-bold ">
-                          {e.title}
-                        </label>
-                        <p className="pt-3 text-base Mobile:text-sm pb-6">
-                          {e.text}
-                        </p>                      
-                        <button className="border-1 px-3 py-1.5 rounded-2xl hover:bg-slate-600 transition-all">
-                          <PrismicNextLink field={e.lermaisbutton}>
-                            <p className="uppercase text-base Mobile:text-sm">
-                              {e.buttontext}
-                            </p>
-                          </PrismicNextLink>
-                        </button>      
-                      </div>
-                    </div>
+              <PrismicNextImage
+                alt=""
+                field={e.data.image}
+                className="object-cover h-full Mobile:aspect-square absolute -z-10"
+              />
+              <div
+                className="flex grow items-center justify-center bg-black bg-opacity-50">
+                  <div className="w-[50%] Mobile:w-[80%] text-center">
+                    <label className="text-3xl Mobile:text-2xl font-bold ">
+                      {e.data.title}
+                    </label>
+                    <p className="pt-3 text-base Mobile:text-sm pb-6">
+                      {e.data.description}
+                    </p>                      
+                    <button className="border-1 px-3 py-1.5 rounded-2xl hover:bg-slate-600 transition-all">
+                      <PrismicNextLink field={e.data.uid}>
+                        <p className="uppercase text-base Mobile:text-sm">
+                          {e.buttontext ||"Ler Mais"}
+                        </p>
+                      </PrismicNextLink>
+                    </button>      
                   </div>
                 </div>
-              </div>
+              </div>    
           );
         })}
       </div>
