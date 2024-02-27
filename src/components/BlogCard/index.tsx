535ec044924809c14d0fe8b75fef59c402283237
EntityBlackHawk <luanf2003@gmail.com>
2024-02-27 15:25:32 -0300

    feat: Add alt text on Blog Page's main image

--- src/components/BlogCard/index.tsx
+++ src/components/BlogCard/index.tsx
@@ -3,6 +3,8 @@ import { PrismicNextImage } from "@prismicio/next";
 import Image from "next/image";
 import CalendarIcon from "public/assets/calendar.svg";
 import TagIcon from "public/assets/tag.svg";
+import HoverTrigger from "../BlogPost/hoverEffect.module.css";
+import Page from "../header";
 
 export type BlogPost = {
   image: ImageField<never> | null;
@@ -26,13 +28,28 @@ export default function BlogCard({
   return (
     <div
       className={
-        "aspect-[395/432] flex flex-col bg-white overflow-clip rounded-lg transition-all hover:drop-shadow-black" +
+        `aspect-[395/432] flex flex-col bg-white overflow-clip rounded-lg transition-all hover:drop-shadow-black ${HoverTrigger.wrapper}` +
         " " +
         className
       }
       onClick={() => onClick()}
     >
-      <PrismicNextImage className="h-1/2 object-cover" field={post.image} />
+      <div className="h-1/2 overflow-hidden grid grid-cols-1 grid-rows-1">
+        <PrismicNextImage
+          className="h-full object-cover row-start-1 row-end-2 col-start-1 col-end-2"
+          field={post.image}
+        />
+        {post.image?.alt && (
+          <div
+            className={
+              "bg-darkgray row-start-1 row-end-2 col-start-1 col-end-2 h-fit self-end opacity-0 translate-y-4 transition-all " +
+              HoverTrigger.target
+            }
+          >
+            <p className="text-white p-2">{post.image?.alt}</p>
+          </div>
+        )}
+      </div>
       <div className="p-6 flex flex-col justify-between h-1/2 border-x border-b border-lightgray rounded-b-lg">
         <div className="bg-white text-black text-base ">
           <h2 className="text-darkblue font-semibold">{post.title}</h2>
@@ -60,9 +77,7 @@ export default function BlogCard({
               width={20}
               height={20}
             />
-            <p>{`${date.at(2)}/${
-              date.at(1)
-            }/${date.at(0)}`}</p>
+            <p>{`${date.at(2)}/${date.at(1)}/${date.at(0)}`}</p>
           </div>
         </div>
       </div>
