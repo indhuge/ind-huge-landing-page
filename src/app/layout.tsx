29cc555c97ea7885be2a43c0047981042b9d34b0
Caiki <caiki2003@gmail.com>
2024-03-27 16:23:56 -0300

    agora vai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const dynamic = 'force-dynamic'
 
 export default async function RootLayout({children}: {children: React.ReactNode} ) {
-  var lang = "pt-br"
-  if (window.location.href.includes("en-us")){
-    lang = "en-us"
-  }
-  else if (window.location.href.includes("es-es")){
-    lang = "es-es"
-  }
   return (
-    <html lang={lang}>
+    <html>
       <body className={inter.className}> 
         {children}
         <PrismicPreview repositoryName={repositoryName} />
