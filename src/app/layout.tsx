4b5870404bfcbc0c3b7815f93ea4f58799365ab3
Caiki <caiki2003@gmail.com>
2024-03-27 16:20:49 -0300

    teste lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,20 @@ import { repositoryName } from "@/prismicio";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { PrismicPreview } from "@prismicio/next";
-import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 export const dynamic = 'force-dynamic'
 
 export default async function RootLayout({children}: {children: React.ReactNode} ) {
+  var lang = "pt-br"
+  if (window.location.href.includes("en-us")){
+    lang = "en-us"
+  }
+  else if (window.location.href.includes("es-es")){
+    lang = "es-es"
+  }
   return (
-    <html>
+    <html lang={lang}>
       <body className={inter.className}> 
         {children}
         <PrismicPreview repositoryName={repositoryName} />
