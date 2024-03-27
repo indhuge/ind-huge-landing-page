4ebe16b1aeeb8ed72fa7e949dcd3f06098ac3f27
Caiki <caiki2003@gmail.com>
2024-03-27 16:15:12 -0300

    teste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,33 +6,14 @@ import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 export const dynamic = 'force-dynamic'
-type Params = { lang : string };
 
-export default async function RootLayout({children, params}: {children: React.ReactNode, params: Params} ) {
+export default async function RootLayout({children}: {children: React.ReactNode} ) {
   return (
-    <html lang={params.lang}>
+    <html>
       <body className={inter.className}> 
         {children}
         <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
   );
-}
-
-export async function generateMetadata({
-  params,
-}: {
-  params: Params;
-}): Promise<Metadata> {
-  return{
-    metadataBase: new URL('https://www.indhuge.com'),
-    alternates: {
-      canonical: '/',
-      languages: {
-        'pt-br': '/',
-        'en-us': '/en-us',
-        'es-es': '/es-es',
-      },
-    },
-  };
 }
